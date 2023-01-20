0f0473ca1db942716184f4519c91560b529f78c0
Masa Orel <orel.masha89@gmail.com>
2023-01-20 09:47:04 +0100

    get location data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import "./scss/style.scss"
+import "./scss/style.scss";
 
+async function getWeatherLocation() {
+  const resource = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=ljubljana&appid=3ba98b4b745fa42c31dff2a783d52b5c&units=metric"
+  );
+  const data = await resource.json();
+  console.log(data);
+}
+
+getWeatherLocation();
