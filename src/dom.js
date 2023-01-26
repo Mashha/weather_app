9791d07b3650f50e9634d081817ef4d865a62a3a
Masa Orel <orel.masha89@gmail.com>
2023-01-26 13:59:21 +0100

    get daily weather - every 3 hours

--- src/dom.js
+++ src/dom.js
@@ -1,35 +1,90 @@
-import { getCurrentWeather, getDailyWeather, getCityCoordinates } from "./weatherAPI.js";
-
+import {
+  getCurrentWeather,
+  getDailyWeather,
+  getCityCoordinates,
+} from "./weatherAPI.js";
 
 export const displayDataOnPage = () => {
-  document.querySelector("form").addEventListener("submit", async (e)=>{
-    const dataCoord = await getCityCoordinates(e)
-    console.log(dataCoord)
-    getCurrentWeather(dataCoord)
-    getDailyWeather(dataCoord)
-  })
+  document.querySelector("form").addEventListener("submit", async (e) => {
+    const dataCoord = await getCityCoordinates(e);
+    const weatherData = await getCurrentWeather(dataCoord);
+    const dailyWeather = await getDailyWeather(dataCoord);
+    console.log(dailyWeather);
+    displayHourlyWeather(dailyWeather);
+
+    displayCityNameAndCountry(weatherData);
+    displayCurrentTemp(weatherData);
+    displayWeatherDescription(weatherData);
+  });
+};
+
+function displayCityNameAndCountry(weatherData) {
+  const cityName = document.createElement("h1");
+  const countryName = document.createElement("span");
+  cityName.textContent = `${weatherData.cityName}, `;
+  countryName.textContent = `${weatherData.country}`;
+
+  const div = document.querySelector(".name-of-the-city");
+  div.append(cityName, countryName);
 }
 
-// function displayCityNameAndCountry(data) {
-//   const cityName = document.createElement("h1");
-//   const countryName = document.createElement("span");
-//   cityName.textContent = `${data.cityName}, `;
-//   countryName.textContent = `${data.country}`;
+function displayCurrentTemp(weatherData) {
+  const p = document.createElement("p");
+  p.textContent = weatherData.currentTemp;
+  const div = document.querySelector(".current-temp");
+  div.append(p);
+}
 
-//   const div = document.querySelector(".name-of-the-city");
-//   div.append(cityName, countryName);
-// }
+function displayWeatherDescription(weatherData) {
+  const p = document.createElement("p");
+  p.textContent = weatherData.currentWeatherDescription;
+  const div = document.querySelector(".description");
+  div.append(p);
+}
 
-// function displayCurrentTemp(data) {
-//   const p = document.createElement("p");
-//   p.textContent = data.currentTemp;
-//   const div = document.querySelector(".current-temp");
-//   div.append(p);
-// }
+function displayHourlyWeather(dailyWeather) {
+  //need current day
+  const today = new Date();
+  const day = today.getDate();
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = months[today.getMonth()];
+  const year = today.getFullYear();
+  const dateForToday = `${day} ${month} ${year}`;
 
-// function displayWeatherDescription(data) {
-//   const p = document.createElement("p");
-//   p.textContent = data.currentWeatherDescription;
-//   const div = document.querySelector(".description");
-//   div.append(p);
-// }
+  //need day from the object
+  dailyWeather.forEach(function (item) {
+    const dateToConvert = item.dt;
+    const newDate = new Date(dateToConvert * 1000);
+    const year = newDate.getFullYear();
+    const month = months[newDate.getMonth()];
+    const date = newDate.getDate();
+    const currentDate = `${date} ${month} ${year}`;
+
+    if (dateForToday === currentDate) {
+      const divHour = document.createElement("div");
+      divHour.classList.add("div-hour");
+      const temp = document.createElement("span");
+      temp.textContent = item.main.temp;
+      const description = document.createElement("div");
+      description.textContent = item.weather[0].description;
+      console.log(item);
+
+      divHour.append(temp, description);
+      const hourlyWeather = document.querySelector(".day-weather-byhours");
+      hourlyWeather.append(divHour);
+    }
+  });
+}
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,84 +1,85 @@
-export async function getCityCoordinates(e){
-  e.preventDefault()
-  const city_name = e.target[0].value
-  if(city_name === ""){
-    return
+export async function getCityCoordinates(e) {
+  e.preventDefault();
+  const city_name = e.target[0].value;
+  if (city_name === "") {
+    return;
   } else {
-  const API_key = "3ba98b4b745fa42c31dff2a783d52b5c";
-  try{
-   const resource = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_key}`)
-   const data = await resource.json()
-   const lat = data[0].lat
-   const lon = data[0].lon
-
-   const coordinates = {
-    lat, lon
-   }
-   console.log(data)
-   console.log(coordinates)
-   return coordinates
-  }
-  catch {
-    (err) => console.log(err)
-  }}
-}
-
-export async function getCurrentWeather(dataCoord) {
- const lat = dataCoord.lat
- const lon = dataCoord.lon
- console.log(lat)
- console.log(lon)
-  const API_key = "3ba98b4b745fa42c31dff2a783d52b5c";
+    const API_key = "3ba98b4b745fa42c31dff2a783d52b5c";
     try {
       const resource = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_key}`
       );
       const data = await resource.json();
-      console.log(data);
-      const cityName = data.name;
-      const country = data.sys.country;
-      const currentTemp = data.main.temp;
-      const feelsLike = data.main.feels_like;
-      const humid = data.main.humidity;
-      const pressure = data.main.pressure;
-      const visibility = data.visibility; //meter
-      const windSpeed = data.wind.speed; // m/s
-      const currentWeather = data.weather[0].main;
-      const currentWeatherDescription = data.weather[0].description;
+      const lat = data[0].lat;
+      const lon = data[0].lon;
 
-      const weatherData = {
-        cityName,
-        country,
-        currentTemp,
-        feelsLike,
-        humid,
-        pressure,
-        visibility,
-        windSpeed,
-        currentWeather,
-        currentWeatherDescription
+      const coordinates = {
+        lat,
+        lon,
       };
-
-      return weatherData;
+      console.log(data);
+      //console.log(coordinates)
+      return coordinates;
     } catch {
       (err) => console.log(err);
     }
   }
+}
 
-export async function getDailyWeather(dataCoord){
-  const lat = dataCoord.lat
-  const lon = dataCoord.lon
+export async function getCurrentWeather(dataCoord) {
+  const lat = dataCoord.lat;
+  const lon = dataCoord.lon;
+  //console.log(lat)
+  //console.log(lon)
   const API_key = "3ba98b4b745fa42c31dff2a783d52b5c";
   try {
     const resource = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`
     );
-    const hourlyData = await resource.json();
-    console.log(hourlyData)
+    const data = await resource.json();
+    console.log(data);
+    const cityName = data.name;
+    const country = data.sys.country;
+    const currentTemp = data.main.temp;
+    const feelsLike = data.main.feels_like;
+    const humid = data.main.humidity;
+    const pressure = data.main.pressure;
+    const visibility = data.visibility; //meter
+    const windSpeed = data.wind.speed; // m/s
+    const currentWeather = data.weather[0].main;
+    const currentWeatherDescription = data.weather[0].description;
+
+    const weatherData = {
+      cityName,
+      country,
+      currentTemp,
+      feelsLike,
+      humid,
+      pressure,
+      visibility,
+      windSpeed,
+      currentWeather,
+      currentWeatherDescription,
+    };
+
+    return weatherData;
   } catch {
     (err) => console.log(err);
   }
 }
 
-
-
+export async function getDailyWeather(dataCoord) {
+  const lat = dataCoord.lat;
+  const lon = dataCoord.lon;
+  const API_key = "3ba98b4b745fa42c31dff2a783d52b5c";
+  try {
+    const resource = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`
+    );
+    const hourlyData = await resource.json();
+    const list = hourlyData.list;
+    return list;
+  } catch {
+    (err) => console.log(err);
+  }
+}
