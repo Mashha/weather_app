9ccb655794b7453e228b740b7cc049449b51e558
Masa Orel <orel.masha89@gmail.com>
2023-02-04 09:08:11 +0100

    geolocation option

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ currentDay();
 
 document.querySelector("form").addEventListener("submit", displayDataOnPage);
 
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function (position) {
-//     var lat = position.coords.latitude;
-//     var long = position.coords.longitude;
-//   });
-// } else {
-  displayDataOnPage({
-    target: [{ value: "Vienna" }],
-    preventDefault: () => {},
-  });
-//}
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log(lat, long)
+  }
+
+  function error(){
+    alert('Unable to retrieve your location');
+    displayDataOnPage({
+      target: [{ value: "Vienna" }],
+      preventDefault: () => {},
+    });
+  }
+} 
+
